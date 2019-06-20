8dfef5599581db06a06f3a2426a02a083c10fc94
yossaj <yossaj@icloud.com>
2019-06-20 14:59:09 +0100

    minor changes

--- src/main/java/Game/Game.java
+++ src/main/java/Game/Game.java
@@ -2,6 +2,7 @@ package Game;
 
 import Game.MythicalCreatures.Dragon;
 import Game.MythicalCreatures.Ogre;
+import Game.Players.Cleric;
 import Game.Players.Knight;
 
 public class Game {
@@ -9,4 +10,5 @@ public class Game {
     private Dragon dragon;
     private Ogre ogre;
     private Knight knight;
+    private Cleric cleric;
 }
