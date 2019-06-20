a53fd2ce17a12a0c7034dfe81b0f008fe1be2445
Alison Williams <alison.lauriewill@gmail.com>
2019-06-20 12:19:16 +0100

    Started Cleric Test

---
+++ src/test/java/ClericTest.java
@@ -0,0 +1,18 @@
+import Game.Players.Cleric;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ClericTest {
+
+    Cleric cleric;
+
+    @Before
+    public void before() {
+        cleric = new Cleric();
+    }
+
+    @Test
+    public void canGetName(){
+        
+    }
+}
