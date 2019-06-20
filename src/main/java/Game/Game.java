31c1c847758ddaa44a4b3aab34f5617d0c366091
yossaj <yossaj@icloud.com>
2019-06-20 14:56:27 +0100

    adding foundations to games file

--- src/main/java/Game/Game.java
+++ src/main/java/Game/Game.java
@@ -1,4 +1,12 @@
 package Game;
 
+import Game.MythicalCreatures.Dragon;
+import Game.MythicalCreatures.Ogre;
+import Game.Players.Knight;
+
 public class Game {
+
+    private Dragon dragon;
+    private Ogre ogre;
+    private Knight knight;
 }
