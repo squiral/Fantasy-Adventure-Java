8868e1c020300212f11b3a54375c38b10ec6a477
yossaj <yossaj@icloud.com>
2019-06-20 12:19:08 +0100

    added mythical creature

--- src/main/java/Game/MythicalCreatures/MythicalCreature.java
+++ src/main/java/Game/MythicalCreatures/MythicalCreature.java
@@ -1,4 +1,25 @@
 package Game.MythicalCreatures;
 
 public abstract class MythicalCreature {
+
+    protected String name;
+    protected Integer health;
+
+    public MythicalCreature(String name, Integer health){
+        this.name = name;
+        this.health = health;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getHealth() {
+        return health;
+    }
+
+    public void takeDamage(int damage) {
+        health += damage;
+    }
+
 }
