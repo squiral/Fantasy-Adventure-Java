4fc74278cca36bf84a1ead4dbf66022c65999dec
James Swatton <jamessswatton@gmail.com>
2019-06-20 12:40:21 +0100

    help me

--- src/main/java/Game/Players/Player.java
+++ src/main/java/Game/Players/Player.java
@@ -1,4 +1,43 @@
 package Game.Players;
 
+import java.util.ArrayList;
+
 public abstract class Player {
+
+    private String name;
+    private int health;
+    private ArrayList<Item> inventory;
+    private int inventorySize;
+    private int carryWeightMax;
+
+    public Player(String name, int health, int inventorySize, int carryWeightMax){
+        this.name = name;
+        this.health = health;
+        this.inventorySize = inventorySize;
+        this.carryWeightMax = carryWeightMax;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getInventorySize() {
+        return inventorySize;
+    }
+
+    public int getCarryWeightMax() {
+        return carryWeightMax;
+    }
+
+    public void addItem(Item item){
+        if(this.inventory.size() < inventorySize) {
+            this.inventory.add(item);
+        }
+    }
+
 }
