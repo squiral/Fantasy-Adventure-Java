88fd360f5dfb111efcf9a2aa9026537ffabb53ef
yossaj <yossaj@icloud.com>
2019-06-20 14:28:45 +0100

    bones for Spell

--- src/main/java/Game/Items/Spell.java
+++ src/main/java/Game/Items/Spell.java
@@ -1,4 +1,14 @@
 package Game.Items;
 
-public class Spell {
+public class Spell extends Item {
+
+    private SpellType type;
+
+    public Spell(int weight, SpellType type) {
+        super(weight);
+        this.type = type;
+    }
+
+    
+
 }
