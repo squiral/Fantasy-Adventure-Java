718e7d303b9abb651f80f6e6716ebcd6280ed349
James Swatton <jamessswatton@gmail.com>
2019-06-21 11:43:37 +0100

    MagicUser Class properties added and Wizard Class created

--- src/main/java/Game/Players/MagicUser.java
+++ src/main/java/Game/Players/MagicUser.java
@@ -1,4 +1,31 @@
 package Game.Players;
 
-public abstract class MagicUser {
+import Game.Behaviours.IAssistable;
+import Game.Items.Spell;
+import Game.Items.SpellType;
+
+import java.util.ArrayList;
+
+public abstract class MagicUser extends Player {
+
+    private ArrayList<Spell> spells;
+    private Spell equippedSpell;
+    private IAssistable creature;
+    private Boolean creatureDefending;
+
+    public MagicUser(String name, ArrayList<Spell> spells, IAssistable creature){
+        super(name, 20, 0);
+        this.spells = spells;
+        this.creature = creature;
+        this.creatureDefending = false;
+    }
+
+    public void equipSpell(SpellType spellType){
+        for(Spell spell : spells){
+            if(spell.type() == spellType){
+                equippedSpell = spell;
+            }
+        }
+    }
+
 }
